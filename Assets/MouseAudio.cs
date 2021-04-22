cad359bb10c853943a43993365661435e80a2457
WuBoytH <alexwuief@gmail.com>
2021-04-22 14:01:12 -0400

    added sounds on click

---
+++ Assets/MouseAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAudio : MonoBehaviour
+{
+    public AudioSource audioS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayClip(AudioClip clip) {
+        audioS.PlayOneShot(clip);
+    }
+}
