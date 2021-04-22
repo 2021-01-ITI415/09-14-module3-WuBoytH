c717ee1cfbc44f9ac6d35147aa6fc0418d7290e0
WuBoytH <alexwuief@gmail.com>
2021-04-22 15:11:45 -0400

    Made audio manager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager manager;
+
+    public void Awake() {
+        manager = this;
+    }
+
+    public AudioSource mainMusic;
+    public AudioSource auxMusic;
+    public AudioSource ambSound;
+
+    
+}
